37ac867284e4fad97bc74ff3e735268f708c0c7d
Murray <murrayadcock@gmail.com>
2019-07-15 16:12:25 +0100

    :lipstick: Adding RSS feed links to footer and updating UI to make room for them

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,7 +11,7 @@ export default (props) => {
         <footer id="footer">
             <section>
                 <h1>Made By Me, But Made Possible By:</h1>
-                <p>CMS: <a href="https://craftcms.com/"><img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/craft-logo.svg" alt="Craft CMS" /></a></p>
+                <p>CMS: <a href="https://craftcms.com/"><img className="wide-logo" src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/craft-logo.svg" alt="Craft CMS" /></a></p>
                 <p>Build: <a href="https://www.gatsbyjs.org/"><img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/gatsby-logo.svg" alt="Gatsby" /></a></p>
                 <p>Deployment: <a href="https://github.com/"><img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/github-logo.svg" alt="GitHub" /></a></p>
                 <p>Hosting: <a href="https://www.netlify.com/"><img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/netlify-logo.svg" alt="Netlify" /></a></p>
@@ -23,7 +23,13 @@ export default (props) => {
                 <p>500px <a href="https://500px.com/theadhocracy"><img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/500px-logo.svg" alt="500px" /></a></p>
                 <p>GitHub <a href="https://github.com/theadhocracy"><img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/github-logo.svg" alt="GitHub" /></a></p>
             </section>
-            {/* RSS Section to go here */}
+            <section>
+                <h1>Keep Up To Date:</h1>
+                <p>All Posts <a href="/rss.xml"><img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/rss-icon.svg" alt="RSS" /></a></p>
+                <p>Articles <a href="/rss-articles.xml"><img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/rss-icon.svg" alt="RSS" /></a></p>
+                <p>Reviews <a href="/"><img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/rss-icon.svg" alt="RSS" /></a></p>
+                <p>Notes <a href="/"><img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/rss-icon.svg" alt="RSS" /></a></p>
+            </section>
             <p className="copyright"><a href="https://www.youtube.com/watch?v=czgOWmtGVGs">&copy; {yearHE} HE</a></p>
         </footer>
     )
