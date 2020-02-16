d4306da7e25877f7f23045afdd6e0e1fafc5f4b2
Murray Adcock <murrayadcock@gmail.com>
2020-02-16 23:15:52 +0000

    :bookmark: Increasing to version 3.1.0

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     siteUrl: `https://www.theadhocracy.co.uk/`,
     siteImage: `static/favicon.svg`,
     twitterHandle: `@theAdhocracy`,
-    version: `3.0.0`
+    version: `3.1.0`
   },
   plugins: [
     {
