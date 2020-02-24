d826cf4b8fac1f8baa8a0f71cafbed2c2d3a62e1
Murray <murrayadcock@gmail.com>
2020-02-24 21:36:07 +0000

    :construction: Populating days in calendar and adding header row

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -6,10 +6,17 @@ import "./calendar.css"
 const Calendar = ({ numDays }) => {
     let calendarDays = []
     for (let i = 0; i < numDays; i++) {
-        calendarDays.push(<li>{i + 1}</li>)
+        calendarDays.push(<li>{i + 1 < 10 ? "0" + (i + 1) : i + 1}</li>)
     }
     return (
-        <ul>
+        <ul className="calendar-list">
+            <li className="calendar-title">M</li>
+            <li className="calendar-title">T</li>
+            <li className="calendar-title">W</li>
+            <li className="calendar-title">T</li>
+            <li className="calendar-title">F</li>
+            <li className="calendar-title">S</li>
+            <li className="calendar-title">S</li>
             {calendarDays}
         </ul>
     )
