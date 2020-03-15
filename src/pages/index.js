e233d846ea0920727cca7ac51625d75906801943
theAdhocracy <murrayadcock@gmail.com>
2020-03-15 16:27:29 +0000

    Initial h-card setup on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,10 @@ import Card from "../components/content_card"
 const IndexPage = ({ data }) => {
     return (
         <Layout title="theAdhocracy" sidebar={false}>
-            <section id="about">
-                <img className={"profile_photo"} src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/profile-photo-square.jpg" alt="Murray Adcock (site owner) wearing shades and raising a single eyebrow." />
+            <section id="about" class="h-card">
+                <img className={"profile_photo u-photo"} src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/profile-photo-square.jpg" alt="Murray Adcock (site owner) wearing shades and raising a single eyebrow." />
                 <h1>oh hi!</h1>
-                <p>I'm afraid you've stumbled into the personal playground of Murray Adcock.</p>
+                <p>I'm afraid you've stumbled into the personal playground of <span class="p-name">Murray Adcock.</span></p>
                 <p>Ad hoc thoughts from an ad hoc mind!</p>
             </section>
             <section id="content">
