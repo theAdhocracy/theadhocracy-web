cca145eacb4aabd278db97fecc26c574d528ebd5
theAdhocracy <murrayadcock@gmail.com>
2020-04-16 10:21:44 +0100

    :zap: Adding bespoke RSS feed for Journal and Notes; updated footer links

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
 	siteMetadata: {
 		title: `theAdhocracy`,
 		description: `Ad hoc thoughts from an ad hoc mind.`,
-		siteUrl: `https://www.theadhocracy.co.uk/`,
+		siteUrl: `https://theadhocracy.co.uk/`,
 		siteImage: `static/favicon.svg`,
 		twitterHandle: `@theAdhocracy`,
 		version: `3.2.4`
@@ -21,17 +21,17 @@ module.exports = {
 			resolve: `gatsby-plugin-feed`,
 			options: {
 				query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
+					{
+						site {
+							siteMetadata {
+								title
+								description
+								siteUrl
+								site_url: siteUrl
+							}
+						}
+					}
+				`,
 				feeds: [
 					{
 						serialize: ({ query: { site, allPosts } }) => {
@@ -46,51 +46,114 @@ module.exports = {
 							})
 						},
 						query: `
-              {
-                allPosts {
-                  edges {
-                    node {
-                      title
-                      slug
-                      snippet
-                      date(formatString: "DD MMM YYYY")
-                    }
-                  }
-                }
-              }
-            `,
+						{
+							allPosts {
+								edges {
+									node {
+										title
+										slug
+										snippet
+										date(formatString: "DD MMM YYYY")
+									}
+								}
+							}
+						}
+						`,
 						output: "/rss.xml",
 						title: "theAdhocracy RSS Feed: All Posts"
 					},
 					{
-						serialize: ({ query: { site, allPosts } }) => {
-							return allPosts.edges.map((edge) => {
+						serialize: ({ query: { site, allArticle } }) => {
+							return allArticle.edges.map((edge) => {
 								return Object.assign({}, edge.node.title, {
 									title: edge.node.title,
 									description: edge.node.snippet,
 									date: edge.node.date,
-									url: site.siteMetadata.siteUrl + "articles/" + edge.node.slug,
-									guid: site.siteMetadata.siteUrl + "articles/" + edge.node.slug
+									url: site.siteMetadata.siteUrl + "/articles/" + edge.node.slug,
+									guid: site.siteMetadata.siteUrl + "/articles/" + edge.node.slug
 								})
 							})
 						},
 						query: `
-              {
-                allPosts {
-                  edges {
-                    node {
-                      title
-                      slug
-                      snippet
-                      date(formatString: "DD MMM YYYY")
-                    }
-                  }
-                }
-              }
-            `,
+							{
+								allArticle {
+									edges {
+										node {
+											title
+											slug
+											snippet
+											date(formatString: "DD MMM YYYY")
+										}
+									}
+								}
+							}
+						`,
 						output: "/rss-articles.xml",
 						title: "theAdhocracy RSS Feed: Articles Only",
 						match: `^.*/article/`
+					},
+					{
+						serialize: ({ query: { site, allJournals } }) => {
+							return allJournals.edges.map((edge) => {
+								return Object.assign({}, edge.node.title, {
+									title: edge.node.title,
+									description: edge.node.snippet,
+									date: edge.node.date,
+									url: site.siteMetadata.siteUrl + "/journal/" + edge.node.year + "/" + edge.node.month + "/" + edge.node.slug,
+									guid: site.siteMetadata.siteUrl + "/journal/" + edge.node.year + "/" + edge.node.month + "/" + edge.node.slug
+								})
+							})
+						},
+						query: `
+							{
+								allJournals {
+									edges {
+										node {
+											title
+											slug
+											snippet
+											date(formatString: "DD MMM YYYY")
+											year
+											month
+										}
+									}
+								}
+							}
+						`,
+						output: "/rss-journal.xml",
+						title: "theAdhocracy RSS Feed: Journal Only",
+						match: `^.*/journals/`
+					},
+					{
+						serialize: ({ query: { site, allNotes } }) => {
+							return allNotes.edges.map((edge) => {
+								return Object.assign({}, edge.node.title, {
+									title: edge.node.title,
+									description: edge.node.snippet,
+									date: edge.node.date,
+									url: site.siteMetadata.siteUrl + "/notes/" + edge.node.slug,
+									guid: site.siteMetadata.siteUrl + "/notes/" + edge.node.slug
+								})
+							})
+						},
+						query: `
+							{
+								allNotes {
+									edges {
+										node {
+											title
+											slug
+											snippet
+											date(formatString: "DD MMM YYYY")
+											
+										}
+									}
+								}
+							}
+						`,
+						output: "/rss-notes.xml",
+						title: "theAdhocracy RSS Feed: Notes Only",
+						match: `^.*/notes/`
 					}
 				]
 			}
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,47 +1,113 @@
-import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 export default (props) => {
+	// Creating Human Era Year
+	const today = new Date()
+	const yearAD = today.getFullYear()
+	const yearHE = (yearAD + 10000).toString().replace(/^([0-9][0-9])/, "$1,")
 
-    // Creating Human Era Year
-    const today = new Date()
-    const yearAD = today.getFullYear()
-    const yearHE = (yearAD + 10000).toString().replace(/^([0-9][0-9])/, "$1,")
+	// Get Version Number
+	const data = useStaticQuery(graphql`
+		query {
+			site {
+				siteMetadata {
+					version
+				}
+			}
+		}
+	`)
+	const version = data.site.siteMetadata.version
 
-    // Get Version Number
-    const data = useStaticQuery(graphql`query {
-        site {
-          siteMetadata {
-            version
-          }
-        }
-      }`)
-    const version = data.site.siteMetadata.version
-
-    return (
-        <footer id="footer">
-            <section>
-                <h1>Made By Me, But Made Possible By:</h1>
-                <p>CMS: <a href="https://craftcms.com/"><img className="wide-logo" src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/craft-logo.svg" alt="Craft CMS" /></a></p>
-                <p>Build: <a href="https://www.gatsbyjs.org/"><img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/gatsby-logo.svg" alt="Gatsby" /></a></p>
-                <p>Deployment: <a href="https://github.com/"><img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/github-logo.svg" alt="GitHub" /></a></p>
-                <p>Hosting: <a href="https://www.netlify.com/"><img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/netlify-logo.svg" alt="Netlify" /></a></p>
-            </section>
-            <section>
-                <h1>Connect With Me:</h1>
-                <p>Twitter <a href="https://twitter.com/theadhocracy" rel="me"><img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/twitter-logo.svg" alt="Twitter" /></a></p>
-                <p>Instagram <a href="https://instagram.com/theadhocracy" rel="me"><img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/instagram-logo.svg" alt="Instragram" /></a></p>
-                <p>500px <a href="https://500px.com/theadhocracy" rel="me"><img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/500px-logo.svg" alt="500px" /></a></p>
-                <p>GitHub <a href="https://github.com/theadhocracy" rel="me"><img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/github-logo.svg" alt="GitHub" /></a></p>
-            </section>
-            <section>
-                <h1>Keep Up To Date:</h1>
-                <p>All Posts <a href="/rss.xml"><img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/rss-icon.svg" alt="RSS" /></a></p>
-                <p>Articles <a href="/rss-articles.xml"><img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/rss-icon.svg" alt="RSS" /></a></p>
-                <p>Reviews <a href="/"><img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/rss-icon.svg" alt="RSS" /></a></p>
-                <p>Notes <a href="/"><img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/rss-icon.svg" alt="RSS" /></a></p>
-            </section>
-            <p className="copyright"><a href="https://www.youtube.com/watch?v=czgOWmtGVGs">&copy; {yearHE} HE</a> <span>|</span> <a href="https://github.com/theAdhocracy/theadhocracy"><span>&#9419;</span> {version}</a></p>
-        </footer>
-    )
+	return (
+		<footer id="footer">
+			<section>
+				<h1>Made By Me, But Made Possible By:</h1>
+				<p>
+					CMS:{" "}
+					<a href="https://craftcms.com/">
+						<img className="wide-logo" src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/craft-logo.svg" alt="Craft CMS" />
+					</a>
+				</p>
+				<p>
+					Build:{" "}
+					<a href="https://www.gatsbyjs.org/">
+						<img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/gatsby-logo.svg" alt="Gatsby" />
+					</a>
+				</p>
+				<p>
+					Deployment:{" "}
+					<a href="https://github.com/">
+						<img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/github-logo.svg" alt="GitHub" />
+					</a>
+				</p>
+				<p>
+					Hosting:{" "}
+					<a href="https://www.netlify.com/">
+						<img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/netlify-logo.svg" alt="Netlify" />
+					</a>
+				</p>
+			</section>
+			<section>
+				<h1>Connect With Me:</h1>
+				<p>
+					Twitter{" "}
+					<a href="https://twitter.com/theadhocracy" rel="me">
+						<img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/twitter-logo.svg" alt="Twitter" />
+					</a>
+				</p>
+				<p>
+					Instagram{" "}
+					<a href="https://instagram.com/theadhocracy" rel="me">
+						<img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/instagram-logo.svg" alt="Instragram" />
+					</a>
+				</p>
+				<p>
+					500px{" "}
+					<a href="https://500px.com/theadhocracy" rel="me">
+						<img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/500px-logo.svg" alt="500px" />
+					</a>
+				</p>
+				<p>
+					GitHub{" "}
+					<a href="https://github.com/theadhocracy" rel="me">
+						<img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/github-logo.svg" alt="GitHub" />
+					</a>
+				</p>
+			</section>
+			<section>
+				<h1>Keep Up To Date:</h1>
+				<p>
+					All Posts{" "}
+					<a href="/rss.xml">
+						<img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/rss-icon.svg" alt="RSS feed." />
+					</a>
+				</p>
+				<p>
+					Articles{" "}
+					<a href="/rss-articles.xml">
+						<img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/rss-icon.svg" alt="RSS feed." />
+					</a>
+				</p>
+				<p>
+					Journal{" "}
+					<a href="/rss-journal.xml">
+						<img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/rss-icon.svg" alt="RSS feed." />
+					</a>
+				</p>
+				<p>
+					Notes{" "}
+					<a href="/rss-notes.xml">
+						<img src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Icons/rss-icon.svg" alt="RSS feed." />
+					</a>
+				</p>
+			</section>
+			<p className="copyright">
+				<a href="https://www.youtube.com/watch?v=czgOWmtGVGs">&copy; {yearHE} HE</a> <span>|</span>{" "}
+				<a href="https://github.com/theAdhocracy/theadhocracy">
+					<span>&#9419;</span> {version}
+				</a>
+			</p>
+		</footer>
+	)
 }
