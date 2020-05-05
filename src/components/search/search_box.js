13af8634d5c08afd14ac357a06566680d9a72e41
theAdhocracy <murrayadcock@gmail.com>
2020-05-05 16:50:57 +0100

    :zap: Adding input targeting to search

--- src/components/search/search_box.js
+++ src/components/search/search_box.js
@@ -3,11 +3,15 @@ import { connectSearchBox } from "react-instantsearch-dom"
 
 import styles from "./search.module.css"
 
+const handleFocus = (event) => event.target.select()
+
 const SearchBox = ({ currentRefinement, isSearchStalled, refine }) => (
-	<form className={styles.search_box} noValidate action="" role="search">
-		<input type="search" value={currentRefinement} onChange={(event) => refine(event.currentTarget.value)} placeholder="Search archives" />
-		<button onClick={() => refine("")}>Reset query</button>
-		{isSearchStalled ? "My search is stalled" : ""}
+	<form className={styles.search_box} noValidate action="javascript:void(0);" role="search" onSubmit={(e) => e.preventDefault()}>
+		<input type="search" value={currentRefinement} onChange={(event) => refine(event.currentTarget.value)} placeholder="Search archives" onClick={handleFocus} />
+		<button onClick={() => refine("")} type="button">
+			🔄
+		</button>
+		{isSearchStalled ? <p>Sorry, search is stalling, please wait a moment.</p> : ""}
 	</form>
 )
 
