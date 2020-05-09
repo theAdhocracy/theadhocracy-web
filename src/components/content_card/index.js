b11657df7334b4a3133dbb86e1145c2cdf064a74
theAdhocracy <murrayadcock@gmail.com>
2020-05-09 20:28:33 +0100

    :rotating_light: Fixing React linting warnings about class/className and list key IDs

--- src/components/content_card/index.js
+++ src/components/content_card/index.js
@@ -4,37 +4,62 @@ import { Link } from "gatsby"
 import "./content_card.css"
 
 const ContentCard = ({ post, type }) => {
-    switch(type){
-        case "journal":
-            return (
-                <section className={"content-card journal-card"}>
-                    <header>
-                        <h2>{post.title}</h2>
-                    </header>
-                    <article dangerouslySetInnerHTML={{ __html: `${post.snippet}` }} />
-                    <footer>
-                        <Link to={`/journal/${post.year}/${post.month.toLowerCase()}/${post.slug}`}><span role="img" aria-label="Book icon">📖</span> Read Entry</Link>
-                        <p><span role="img" title="Date published" aria-label="Date published">📆</span> {post.date}</p>
-                    </footer>
-                </section>
-            )
-        default:
-            return (
-                <section className={"content-card"}>
-                    <header>
-                        <h2>{post.title}</h2>
-                    </header>
-                    <article dangerouslySetInnerHTML={{ __html: `${post.snippet}` }} />
-                    <footer>
-                        <Link to={`/article/${post.slug}`}><span role="img" aria-label="Book icon">📖</span> Read Article</Link>
-                        <p><span role="img" title="Date published" aria-label="Date published">📆</span> {post.date}</p>
-                        <p><span>Categories</span></p>
-                        <ul className="flat-list">{post.categories.map(category => <li>{category}</li>)}</ul>
-                    </footer>
-                </section>
-            )
-    }
+	switch (type) {
+		case "journal":
+			return (
+				<section className={"content-card journal-card"}>
+					<header>
+						<h2>{post.title}</h2>
+					</header>
+					<article dangerouslySetInnerHTML={{ __html: `${post.snippet}` }} />
+					<footer>
+						<Link to={`/journal/${post.year}/${post.month.toLowerCase()}/${post.slug}`}>
+							<span role="img" aria-label="Book icon">
+								📖
+							</span>{" "}
+							Read Entry
+						</Link>
+						<p>
+							<span role="img" title="Date published" aria-label="Date published">
+								📆
+							</span>{" "}
+							{post.date}
+						</p>
+					</footer>
+				</section>
+			)
+		default:
+			return (
+				<section className={"content-card"}>
+					<header>
+						<h2>{post.title}</h2>
+					</header>
+					<article dangerouslySetInnerHTML={{ __html: `${post.snippet}` }} />
+					<footer>
+						<Link to={`/article/${post.slug}`}>
+							<span role="img" aria-label="Book icon">
+								📖
+							</span>{" "}
+							Read Article
+						</Link>
+						<p>
+							<span role="img" title="Date published" aria-label="Date published">
+								📆
+							</span>{" "}
+							{post.date}
+						</p>
+						<p>
+							<span>Categories</span>
+						</p>
+						<ul className="flat-list">
+							{post.categories.map((category, index) => (
+								<li key={index}>{category}</li>
+							))}
+						</ul>
+					</footer>
+				</section>
+			)
+	}
 }
 
 export default ContentCard
-
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,34 +5,56 @@ import Layout from "../components/layout"
 import Card from "../components/content_card"
 
 const IndexPage = ({ data }) => {
-    return (
-        <Layout title="theAdhocracy" sidebar={false}>
-            <section id="about" class="h-card">
-                <a class="u-url u-uid" href="https://theadhocracy.co.uk" rel="me"><img className={"profile_photo u-photo"} src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/profile-photo-square.jpg" alt="Murray Adcock (site owner) wearing shades and raising a single eyebrow." /></a>
-                <h1>oh hi!</h1>
-                <p>I'm afraid you've stumbled into the personal playground of <span class="p-name">Murray Adcock.</span></p>
-                <p class="p-note">Ad hoc thoughts from an ad hoc mind!</p>
-            </section>
-            <section id="content">
-                <header>
-                    <h1>What I'm Up To</h1>
-                </header>
-                <noscript>
-                    <p><strong>JavaScript is Disabled - Cannot Load Latest Posts</strong></p>
-                    <p>Sorry, I realise this is a bit rough, but currently articles on theAdhocracy only work with JavaScript. It's something I'm actively looking to address in the future, but right now I'm struggling to find a solution.</p>
-                    <p>If you're interested, the reason for the requirement is because the site is hosted on <a href="https://www.netlify.com" target="_blank" rel="noopener noreferrer">Netlify</a> but dynamically loads content in from a <a href="https://craftcms.com/" target="_blank" rel="noopener noreferrer">Craft CMS</a> instance on an old-school webserver. That leaves me with a choice: client-side content rendering (aka JavaScript) or forcing a rebuild of the whole site for every edit. Neither seem particularly viable, so right now I've chosen JavaScript.</p>
-                    <p>If you have any ideas or suggestions, I'm always happy to hear them. Feel free to DM or @ me on Twitter <a href="https://twitter.com/theadhocracy" target="_blank" rel="noopener noreferrer">@theAdhocracy</a> with your thoughts 👍</p>
-                </noscript>
-                <main className="content-grid">
-                    {/* TODO: Add Notes back onto homepage but in a more simplified manner */}
-                    {data.allPosts.nodes.map(node => (
-                            <Card post={node} type={node.contentType === "journals" ? "journal" : ""} />
-                    ))}
-                    <a href="/articles/">Explore?</a>
-                </main>
-            </section>
-        </Layout>
-    )
+	return (
+		<Layout title="theAdhocracy" sidebar={false}>
+			<section id="about" className="h-card">
+				<a className="u-url u-uid" href="https://theadhocracy.co.uk" rel="me">
+					<img className={"profile_photo u-photo"} src="https://cms.theadhocracy.co.uk/assets/theadhocracy/website/profile-photo-square.jpg" alt="Murray Adcock (site owner) wearing shades and raising a single eyebrow." />
+				</a>
+				<h1>oh hi!</h1>
+				<p>
+					I'm afraid you've stumbled into the personal playground of <span className="p-name">Murray Adcock.</span>
+				</p>
+				<p className="p-note">Ad hoc thoughts from an ad hoc mind!</p>
+			</section>
+			<section id="content">
+				<header>
+					<h1>What I'm Up To</h1>
+				</header>
+				<noscript>
+					<p>
+						<strong>JavaScript is Disabled - Cannot Load Latest Posts</strong>
+					</p>
+					<p>Sorry, I realise this is a bit rough, but currently articles on theAdhocracy only work with JavaScript. It's something I'm actively looking to address in the future, but right now I'm struggling to find a solution.</p>
+					<p>
+						If you're interested, the reason for the requirement is because the site is hosted on{" "}
+						<a href="https://www.netlify.com" target="_blank" rel="noopener noreferrer">
+							Netlify
+						</a>{" "}
+						but dynamically loads content in from a{" "}
+						<a href="https://craftcms.com/" target="_blank" rel="noopener noreferrer">
+							Craft CMS
+						</a>{" "}
+						instance on an old-school webserver. That leaves me with a choice: client-side content rendering (aka JavaScript) or forcing a rebuild of the whole site for every edit. Neither seem particularly viable, so right now I've chosen JavaScript.
+					</p>
+					<p>
+						If you have any ideas or suggestions, I'm always happy to hear them. Feel free to DM or @ me on Twitter{" "}
+						<a href="https://twitter.com/theadhocracy" target="_blank" rel="noopener noreferrer">
+							@theAdhocracy
+						</a>{" "}
+						with your thoughts 👍
+					</p>
+				</noscript>
+				<main className="content-grid">
+					{/* TODO: Add Notes back onto homepage but in a more simplified manner */}
+					{data.allPosts.nodes.map((node, index) => (
+						<Card key={index} post={node} type={node.contentType === "journals" ? "journal" : ""} />
+					))}
+					<a href="/articles/">Explore?</a>
+				</main>
+			</section>
+		</Layout>
+	)
 }
 
 export default IndexPage
@@ -41,16 +63,16 @@ export const query = graphql`
 	query LatestPostsQuery {
 		allPosts {
 			nodes {
-                title
-                slug
-                snippet
-                categories
-                tags
-                date(formatString: "DD MMM YYYY")
-                month
-                year
-                contentType
-            }
+				title
+				slug
+				snippet
+				categories
+				tags
+				date(formatString: "DD MMM YYYY")
+				month
+				year
+				contentType
+			}
 		}
 	}
 `
