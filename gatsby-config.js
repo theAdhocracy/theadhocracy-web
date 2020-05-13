34661d0fa4661d391965edd35286f14afc42885f
theAdhocracy <murrayadcock@gmail.com>
2020-05-13 19:33:31 +0100

    :bookmark: Release 3.3.4

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
 		siteUrl: `https://theadhocracy.co.uk/`,
 		siteImage: `static/favicon.svg`,
 		twitterHandle: `@theAdhocracy`,
-		version: `3.3.3`
+		version: `3.3.4`
 	},
 	plugins: [
 		{
