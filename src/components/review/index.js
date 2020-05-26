1bccbc27a5f4ac63698c6851e3296556d6b3b234
theAdhocracy <murrayadcock@gmail.com>
2020-05-26 16:52:05 +0100

    :sparkles New review component

---
+++ src/components/review/index.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+import Rating from "../rating"
+
+const Review = ({ review }) => {
+	return (
+		<article className="full-width">
+			{review.title ? <h2 id={review.title.toLowerCase()}>{review.title}</h2> : <h2>Review</h2>}
+			<aside>
+				{review.rewatchList.length > 0 && <span>#1</span>}
+				<span>{review.date}</span>
+				<Rating value={review.rating} />
+				{review.location && <span>{review.location}</span>}
+			</aside>
+			<div id="article-body" className="e-content" dangerouslySetInnerHTML={{ __html: review.copy }} />
+			{review.rewatchList.map((rewatch, index) => {
+				return (
+					<section key={index}>
+						<h3>Rewatch</h3>
+						<aside>
+							<span>#{index + 2}</span>
+							<span>{rewatch.date}</span>
+							<Rating value={rewatch.rating} />
+							{rewatch.location && <span>{rewatch.location}</span>}
+						</aside>
+						<div id="article-body" className="e-content" dangerouslySetInnerHTML={{ __html: rewatch.copy }} />
+					</section>
+				)
+			})}
+		</article>
+	)
+}
+
+export default Review
