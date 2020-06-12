3067a1714022e14b18215d06d5af6a9dc3138253
theAdhocracy <murrayadcock@gmail.com>
2020-06-12 12:05:01 +0100

    :lipstick: Fixing wrapping on 5.5 and 6 star ratings; generally improving design breakpoints

--- src/components/rating/index.js
+++ src/components/rating/index.js
@@ -1,8 +1,9 @@
 import React from "react"
+import styles from "./rating.module.css"
 
 const Rating = ({ value }) => {
 	return (
-		<span role="img" aria-label={`Rated ${value} out of 5`}>
+		<span className={value > 5 && styles.superb} role="img" aria-label={`Rated ${value} out of 5`}>
 			{"⭐".repeat(value)}
 			{value % 1 !== 0 ? "½" : null}
 		</span>
