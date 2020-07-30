d37ed18975781e6f400e7395d5e92802fabb37d7
theAdhocracy <murrayadcock@gmail.com>
2020-07-30 12:40:49 +0100

    :bookmark: Release 3.5.0 [skip netlify]

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
 		siteUrl: `https://theadhocracy.co.uk`,
 		siteImage: `https://cms.theadhocracy.co.uk/assets/theadhocracy/website/Logos/adhoc-face.svg`,
 		twitterHandle: `@theAdhocracy`,
-		version: `3.4.3`
+		version: `3.5.0`
 	},
 	plugins: [
 		{
